7167c983e7300b7f81f346990d86736871a90bee
lorenzkieran <lorenz.hoffmann03@gmail.com>
2026-03-01 19:46:01 +0100

    Init Springboot-Backend

---
+++ backend/src/main/java/com/pm4/istp/IstpApplication.java
@@ -0,0 +1,13 @@
+package com.pm4.istp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IstpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IstpApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/pm4/istp/IstpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm4.istp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IstpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
