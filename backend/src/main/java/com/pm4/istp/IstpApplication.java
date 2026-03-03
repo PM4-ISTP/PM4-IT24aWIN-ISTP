689a7a413bcea2135e7f1d01a3939b33ce8302b0
Alessio Seeberger <aseeberger@me.com>
2026-03-03 11:55:40 +0100

    feat: integrate backend linting and formatting tools with CI pipeline

--- backend/src/main/java/com/pm4/istp/IstpApplication.java
+++ backend/src/main/java/com/pm4/istp/IstpApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class IstpApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IstpApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(IstpApplication.class, args);
+  }
 }
