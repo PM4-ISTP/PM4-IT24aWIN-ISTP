f092ffaaa5a2968f60195d8c2a85fde5b5e25be3
Calabrese Davide <calabdav@students.zhaw.ch>
2026-04-02 20:09:58 +0200

    Revert "debug2"
    This reverts commit b4fb67e6888ef0e5b38317322a0185f647f703eb.

--- frontend/src/app/dashboard/instructor/[id]/page.tsx
+++ frontend/src/app/dashboard/instructor/[id]/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-console.log("COMPONENT RENDERE");
-
 import { useEffect, useState } from "react";
 import { useParams, useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
