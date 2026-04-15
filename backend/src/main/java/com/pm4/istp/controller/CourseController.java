b449774bfa6c376677d3171b99de9bdd3b67cc95
Lemasu <75743436+Lemasu@users.noreply.github.com>
2026-04-15 19:38:33 +0200

    Format code using Spotless

--- backend/src/main/java/com/pm4/istp/controller/CourseController.java
+++ backend/src/main/java/com/pm4/istp/controller/CourseController.java
@@ -18,9 +18,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
