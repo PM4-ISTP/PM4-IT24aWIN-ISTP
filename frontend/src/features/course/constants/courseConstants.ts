08e301b635753bf5a57cc151ddf9a749f9258de4
Biedermann Linus (biedeli1) <biedeli1@students.zhaw.ch>
2026-04-26 16:49:23 +0200

    Delete courseConstant.ts

--- frontend/src/features/course/constants/courseConstants.ts
+++
@@ -1,5 +0,0 @@
-export const DIFFICULTY_OPTIONS = [
-  { value: "BEGINNER", label: "Beginner" },
-  { value: "INTERMEDIATE", label: "Intermediate" },
-  { value: "ADVANCED", label: "Advanced" },
-];
