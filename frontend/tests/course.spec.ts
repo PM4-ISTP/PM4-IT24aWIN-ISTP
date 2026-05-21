8d86476b48df9a9a471c3395906b008d7dc1fd0f
Lemasu <75743436+Lemasu@users.noreply.github.com>
2026-05-22 01:42:21 +0200

    Delete unnecessary click

--- frontend/tests/course.spec.ts
+++ frontend/tests/course.spec.ts
@@ -44,7 +44,6 @@ test("Instructor can create a course and edit it afterwards (e.g. change title, 
     .fill(newCourseData.description);
   await page.getByRole("combobox", { name: "Topic" }).click();
   await page.getByRole("listbox").getByText(newCourseData.topic).click();
-  await page.getByRole("combobox", { name: "Collaborators" }).click();
   await page.locator("label").filter({ hasText: "Public" }).click();
   await page.locator("label").filter({ hasText: "Once" }).click();
   await clickButtonAndAssert(
