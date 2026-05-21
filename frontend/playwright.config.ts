6a921fd461d1ad3d52fa4c9321dc1b4e0ba28ed1
Lemasu <75743436+Lemasu@users.noreply.github.com>
2026-05-22 01:11:52 +0200

    Default timeout was too low

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -16,6 +16,8 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: 1,
+  /* Increase default timeout */
+  timeout: 60_000,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
